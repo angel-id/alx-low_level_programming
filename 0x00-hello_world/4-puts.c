d33e1e12180dc49c9d6af4e698567348ce391065
Idoko Angel <angelidokoangel@gmail.com>
2022-09-12 19:48:33 -0700

    a program that writes a texts and prints new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,18 +3,14 @@
 
 
 /**
- *
- *  * main -prints a certain string
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *  main - prints a statement
+ *  Return: Always 0 (Success)   
+ */
 
 int main(void)
 
 {
             puts("\"Programming is like building a multilingual puzzle");
 	    return (0);
+
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+/**
+ *  main - prints a phrase
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+           printf("\"with proper grammar, but the outcome is a piece of art,")
+	   return (0);
+
+}
