d183f4a292a5087b706a39b94aebba660228f559
Idoko Angel <angelidokoangel@gmail.com>
2022-09-18 06:51:45 -0700

    a program that prints lower and upper case alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - to print lower case alphabets except c and q
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+{	putchar(ch)
+	
+	if (ch != 'e' && ch != 'q')
+{	putchar(ch)
+}
+	ch++;
+}
+
+putchar('\n');
+return (0);
+}
+
