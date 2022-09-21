621c57c145496c4a1289768de7069a259f8a2728
Idoko Angel <angelidokoangel@gmail.com>
2022-09-21 16:31:28 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - a function that checks for alphabetic character
+ * Returns: 1 if c is a letter, lowercase or uppercase 
+ * otherwise 0
+ * @c: char in letters
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	
+	else
+	{
+		return (0);
+		_putchar('\n');
+
