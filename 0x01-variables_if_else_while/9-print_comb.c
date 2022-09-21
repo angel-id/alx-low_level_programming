75eddc3eba057a3ab58ad9f4b666e8c921f504e2
Idoko Angel <angelidokoangel@gmail.com>
2022-09-21 09:41:41 -0700

    a program that prints all the numbers of base 16 in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - a program that prints
+ * every single combination of
+ * single digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+	for (x = 0; x < 10; x++)
+	{
+		putchar(c);
+		if (c < 9)
+		{
+			putchar(3);
+			putchar(-14);
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
