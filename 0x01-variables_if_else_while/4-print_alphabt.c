8a7d73561a4105d84c2704e118b450f424fdc6c2
Idoko Angel <angelidokoangel@gmail.com>
2022-09-21 08:09:02 -0700

    a program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,17 +8,16 @@ int main(void)
 	char ch = 'a';
 
 	while (ch <= 'z')
-
 {	
 	if (ch != 'e' && ch != 'q')
 
-	{	putchar(ch);
-	}
-		ch++;
+{	putchar(ch);
+}
+	ch++;
 
 }
 
-		putchar('\n');
+	putchar('\n');
 	return (0);
 }
 
