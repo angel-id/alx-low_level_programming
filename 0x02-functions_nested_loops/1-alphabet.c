faab6d73dbfbc7b26a1a9c032d76e83f82254bc7
Idoko Angel <angelidokoangel@gmail.com>
2022-09-21 14:14:19 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - a program that prints the 
+ * alphabet in lowercase using '_putchar'
+ * Return: 0
+ */
+
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		char alpha;
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchat(alpha);
+		}
+	}
+		_putchar('\n');
+		return (0);
+}
