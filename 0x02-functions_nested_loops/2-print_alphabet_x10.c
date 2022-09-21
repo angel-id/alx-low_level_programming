52e699d8b86b33312bd742681831a44e4727f881
Idoko Angel <angelidokoangel@gmail.com>
2022-09-21 15:10:56 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a program that prints ten times
+ * the alphabet in lowercase followed by a new line
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int b;
+
+	while (b = 0; b <= 9; b++)
+	{
+		while (a = 'a'; a <= 'z'; a++)
+		{
+			putchar(a);
+		}
+
+		putchar('\n')
+	}
+}
+	
