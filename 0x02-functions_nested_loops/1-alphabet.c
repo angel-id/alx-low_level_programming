85a5ee9cc33e35332c84e98c90a65790fc3c9a1f
Idoko Angel <angelidokoangel@gmail.com>
2022-09-21 14:55:15 -0700

    initial commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet - a program that prints the 
- * alphabet in lowercase using '_putchar'
+ * print_alphabet - a program that prints the alphabet
+ * in lowercase using '_putchar'
  * Return: 0
  */
 
 void print_alphabet(void)
 {
 	char alpha;
-
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
 	}
-
 	_putchar('\n');
 }
