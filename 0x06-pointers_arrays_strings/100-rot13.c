28a4c6b8d7d9148a8c4154627613d5c8975ff3c1
Idoko Angel <angelidokoangel@gmail.com>
2022-09-22 04:35:37 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string to be encoded
+ * Return: string
+ */
+
+char *rot13(char *);
+{
+	int u, v;
+	char x[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char y[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (u = 0; s[u] != '\0'; u++)
+	{
+		for (v = 0; s[v] != '\0'; v++)
+		{
+			if  (s[u] == x[v])
+			{
+				s[u] == y[v];
+				break;
+			}
+		}
+	}
+	return (s);
+}
