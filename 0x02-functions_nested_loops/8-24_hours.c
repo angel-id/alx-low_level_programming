b81bd558feea0242c2e22da855fbdc7570095413
Idoko Angel <angelidokoangel@gmail.com>
2022-09-22 08:43:15 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+	
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+	
+
+
