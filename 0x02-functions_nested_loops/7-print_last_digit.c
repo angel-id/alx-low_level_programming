21da40b2b411b5407bc5d5a100dc7b985557e778
Idoko Angel <angelidokoangel@gmail.com>
2022-09-22 08:29:32 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of a number
+ * @y: input number as int
+ * Return: last digit of function
+ */
+
+int print_last_digit(int y)
+{
+	int i;
+	i = y % 10;
+	if (i < 0)
+	{
+		_putchar(-i + '0')
+		return (-i)
+	}
+	else
+	{
+		_putchar(i + '0');
+		return (i);
+	}
+}
