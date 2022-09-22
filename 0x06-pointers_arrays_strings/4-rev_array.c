3b629344e29b5c9b4b981e6ea1df55dd1f9e8efb
Idoko Angel <angelidokoangel@gmail.com>
2022-09-22 03:10:47 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the contents of an array of integers
+ * @a: array 
+ * @n: number of elements in array
+ * Return: 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < (n / 2); i++)
+	{	j = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = j;
+	}
+}
