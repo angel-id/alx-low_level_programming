b5d222489a2582c4e4eed43c2418783a0a5639a4
Idoko Angel <angelidokoangel@gmail.com>
2022-09-21 18:12:27 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{	char ggg;
+	int i, j, k;
+	j = 0;
+	k = 0;
+
+	while (s[j] != '\0')				
+	{
+		j++;
+	}
+	k = j - 1;
+
+	for (i = 0; i < j / 2; i++)
+	{
+		ggg = s[i];
+		s[i] = s[k];
+		s[k--] = ggg;
+	}
+}
