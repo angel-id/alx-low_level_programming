10832f6fc72563dad8e0180d9a736ecebf4ab92f
Idoko Angel <angelidokoangel@gmail.com>
2022-09-22 10:08:21 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 50 fibonacci numbers
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+	long int num1, num2, fibn;
+	num1 = 1;
+	num2 = 2;
+	printf("%ld, %ld", num1, num2);
+	for (i = 0; i < 48; i++)
+	{
+		fibn = num1 + num2;
+		printf(", %ld", fibn);
+		num1 = num2;
+		num2 = fibn;
+	}
+	printf("\n");
+	return (0);
+}
