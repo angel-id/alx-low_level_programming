7f7c1ffcd86b12e7ea8c5115dd973125eef22a7f
Idoko Angel <angelidokoangel@gmail.com>
2022-09-22 16:29:35 -0700

    initial commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - a program that checks if an
+ * integer is negative or positive
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+	positive_or_negative(x);
+
+	return (0);
+}
