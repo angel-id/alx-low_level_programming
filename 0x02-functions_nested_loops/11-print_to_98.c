1affb65067a6d38e9b8fd07e7165d0261f00fc75
Idoko Angel <angelidokoangel@gmail.com>
2022-09-22 09:38:01 -0700

    initial commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_to_98 -  a function that prints all natural numbers from n to 98, followed by a new line
+ * print_to_98 -  a function that prints all natural numbers from n to 98,
+ * followed by a new line
  * @i: int type number
  */
 
@@ -15,7 +16,7 @@ void print_to_98(int i)
 			printf("%d, ", i);
 		}
 	}
-	else if(i < 98)
+	else if (i < 98)
 	{
 		for (; i < 98; i++)
 		{
