c5e6eef8810e90de3546fa3669892fa15d34c9c0
Idoko Angel <angelidokoangel@gmail.com>
2022-09-21 17:41:08 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string input to evaluate
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+	{
+		i++;
+	}
+	return (n);
+}
