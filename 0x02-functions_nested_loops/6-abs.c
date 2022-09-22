7b5d51c637567b735fe4785c5e94bc6f05ea957f
Idoko Angel <angelidokoangel@gmail.com>
2022-09-22 08:13:44 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - a function that prints absolute value of an integer
+ * @a: the int to input
+ * Return: absolute value
+ */
+ 
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else 
+	{
+		return (a * -1);
+	}
+}
