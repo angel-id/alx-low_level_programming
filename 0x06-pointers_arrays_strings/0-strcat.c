edbdae6f2f756ae0cee80de650a32731aac07cc8
Idoko Angel <angelidokoangel@gmail.com>
2022-09-21 19:41:35 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: char string to concatenate to / destination
+ * @src: source
+ * Return: pointer to dest
+ */
+
+
+
+char *_strcat(char *dest, char *src)
+{
+	int a, c;
+
+	for (a = 0; dest[a] != '\0'; a++)						for (c = 0; src[c] != '\0'; c++)
+	{	
+		dest[a] = src[c];
+		a++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
+}
