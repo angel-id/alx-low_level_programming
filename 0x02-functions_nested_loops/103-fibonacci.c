dcc1615feb898cd60eae2393833bdfd8693560f1
Idoko Angel <angelidokoangel@gmail.com>
2022-09-22 10:16:28 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the add of the even-valued
+ * fibonacci numbers.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int num1, num2, fibn, afibn;
+	num1 = 1;
+	num2 = 2;
+	fibn = afibn = 0;
+	while (fibn <= 4000000)
+	{
+		fibn = num1 + num2;
+		num1 = num2;
+		num2 = fibn;
+		if ((num1 % 2) == 0)
+		{
+			afibn += num1;
+		}
+	}
+	printf("%ld\n", afibn);
+	return (0);
+}
