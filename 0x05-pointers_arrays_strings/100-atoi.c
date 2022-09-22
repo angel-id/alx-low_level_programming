65660e2a49554b3a9a20aec4176d3d40e3cda080
Idoko Angel <angelidokoangel@gmail.com>
2022-09-21 19:17:46 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: the string to be converted to an integer
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+		int count = 1;
+		unsigned int num = 0;
+
+	do{
+	if (*s == '-')
+	count *= -1;
+	else if (*s >= '0' && *s <= '9')
+		num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+	{
+	return (num * count);
+	}
+}
