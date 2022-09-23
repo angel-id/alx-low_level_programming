ab43bac35811cb2497908bba63b2c1a7ef01e97e
Idoko Angel <angelidokoangel@gmail.com>
2022-09-22 18:02:07 -0700

    intial commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers from 0
+ * to 9, except 2 and 4, with a new line
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i + 48);
+		}
+	}
+
+	_putchar('\n');
+}
