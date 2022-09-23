5daa6408a09202bd15198fd9dab3d704162dbb49
Idoko Angel <angelidokoangel@gmail.com>
2022-09-22 18:48:10 -0700

    intial commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - a function that draws a 
+ * diagonal line on the terminal
+ * @n: number of times diagonal should be printed
+ * Return: nothing
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		putchar ('\n');
+	}
+	else
+	{
+		int l, m;
+
+		for (l = 0; l < n; l++)
+		{
+			for (m = 0; m < n; m++)
+			{
+				if (m == l)
+					_putchar('\\');
+				else if (m < l)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
+		
