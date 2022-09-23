7de19c85c3e9501daa616a2f5b3ab1d9221e9037
Idoko Angel <angelidokoangel@gmail.com>
2022-09-22 17:13:52 -0700

    initial commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,18 +7,16 @@
  * Return: pointer to dest
  */
 
-
-
 char *_strcat(char *dest, char *src)
 {
 	int a, c;
 
-	for (a = 0; dest[a] != '\0'; a++)						for (c = 0; src[c] != '\0'; c++)
-	{	
+	for (a = 0; dest[a] != '\0'; a++)
+		for (c = 0; src[c] != '\0'; c++)
+		{	
 		dest[a] = src[c];
 		a++;
-	}
-
+		}
 	dest[a] = '\0';
 	return (dest);
 }
