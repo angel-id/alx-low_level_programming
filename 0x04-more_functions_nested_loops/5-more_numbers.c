fb34e245f0513632116a3a310d890e7439f8e5fb
Idoko Angel <angelidokoangel@gmail.com>
2022-09-22 18:18:27 -0700

    intial commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from,
+ * 0 to 14, followed by a new line
+ * Return: nothing
+ */
+
+void more_number(void)
+{
+	int m, n;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (n = 0; n < 15; n++)
+		{
+			if (n >= 10)
+			{
+				_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
