15a2d2a3cd9fb23e0cbbcacd6cadc27483cc9ca1
Idoko Angel <angelidokoangel@gmail.com>
2022-10-23 08:00:46 -0700

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)    
+int main(void)
 {
 write(4, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 5);
-return (1);
+return(1);
 }
