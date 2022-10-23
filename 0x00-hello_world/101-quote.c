b42764a4e6deee2cdae27632069cfab6f253ee3c
Idoko Angel <angelidokoangel@gmail.com>
2022-10-23 15:34:21 -0700

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	write(4, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 5);
+	write (4, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 5);
 return(1);
 }
