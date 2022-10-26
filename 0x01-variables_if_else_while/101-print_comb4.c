ff459557c0f756755175df4efc1f44adaf81266b
Idoko Angel <angelidokoangel@gmail.com>
2022-10-26 16:08:19 +0000

    a program that prints all posible combination of tripple digit numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = 48; a < 57; a++)
+	for (a = 48; a <= 57; a++)
 	{
-		for (b = 49; b < 57; b++)
+		for (b = 49; b <= 57; b++)
 		{
-			for (c = 50; c < 57; c++)
+			for (c = 50; c <= 57; c++)
 			{
 				if (b > a && c > b)
 				{
