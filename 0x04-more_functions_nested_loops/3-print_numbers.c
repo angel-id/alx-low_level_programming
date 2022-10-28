a7838be7bc6636f8b3bc712f3720997309b1e33e
Idoko Angel <angelidokoangel@gmail.com>
2022-10-28 13:05:33 +0000

    a function that prints numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /*
- * main - a function that prints numbers 0 to 9
+ * print_numbers - a function that prints numbers 0 to 9
  * followed by a new line
  * Return: nothing
  */
-
 void print_numbers(void)
 {
 	int n;
-
+	
 	for(n = 0; n < 10; n++)
 		_putchar(n + 48);
 	_putchar('\n');
