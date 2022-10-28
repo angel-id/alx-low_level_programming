e53b1266ed75560916fe622dfc344fa371b0ed56
Idoko Angel <angelidokoangel@gmail.com>
2022-10-28 12:45:58 +0000

    a function that prints numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -11,7 +11,12 @@ void print_numbers(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
-		putchar(n + 48);
-	putchar('\n');
+	n = 0;
+
+	for (n < 10)
+	{
+		n++;
+		_putchar(n + 48);
+	}
+	_putchar('\n');
 }
