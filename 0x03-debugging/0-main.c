dd9f34d149c2171d89db2ad6a46c8ce7ba007c77
Idoko Angel <angelidokoangel@gmail.com>
2023-03-30 14:15:07 +0000

    show that the function gives the correct output when given the case 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * main - a program that checks if an
- * integer is negative or positive
+ * main - this file must test that the
+ * function positive_or_negative() gives
+ * the correct output when given the case of '0'.
  * Return: 0
  */
 
 int main(void)
 {
 	int x;
+
 	x = 0;
-	
 	positive_or_negative(x);
-	
+
 	return (0);
 }
