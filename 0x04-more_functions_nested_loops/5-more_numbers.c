bf6a62d2ea1d99cead9a98b8af55adefe3caa1d4
Idoko Angel <angelidokoangel@gmail.com>
2023-04-08 18:02:55 +0000

    prints 1 to 14 on a new line ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,18 +8,18 @@
 
 void more_numbers(void)
 {
-        int i, j;
-	
+	int i, j;
+
 	i = 0;
 	while (i < 10)
-        {
+	{
 		for (j = 0; j < 15; j++)
-                {
-                        if (j >= 10)
-                                _putchar(j / 10 + '0');
-                        _putchar(j % 10 + '0');
-                }
-                _putchar('\n');
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
 		i++;
-        }
+	}
 }
