e57a2099efeef441bf21033053639efcbfe6f4a1
Idoko Angel <angelidokoangel@gmail.com>
2023-06-14 00:25:35 +0000

    a program that prints the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints number of arguments
+ * passed to it.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
