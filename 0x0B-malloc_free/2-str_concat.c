3a587cf0d10db978e0cb7936667fb3b82623987d
Idoko Angel <angelidokoangel@gmail.com>
2023-06-20 14:18:45 +0000

    a program that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,9 +22,9 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	/* get the size of the strings*/
 
-	for (i = 0; i < s1[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		;
-	for (j = 0; j < s2[j] != '\0'; j++)
+	for (j = 0; s2[j] != '\0'; j++)
 		;
 	/*allocate memory for both strings s1 and s2*/
 
