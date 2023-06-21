37a29d3cdbd57dd3ac3540cddcd75ee7c0f400c0
Idoko Angel <angelidokoangel@gmail.com>
2023-06-21 00:13:38 +0000

    a program that prints file name it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * main - a program that prints the name of
+ * file it was compiled from
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
