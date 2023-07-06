5b589b07da6459ec5c17f80b46d7f2fc8b8664ba
Idoko Angel <angelidokoangel@gmail.com>
2023-07-06 02:58:02 +0000

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop contained
+ * in a linked list
+ * @head : head pointer
+ *
+ * Return: pointer the node where the loop starts,
+ * or NULL if there is no loop
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *fnode, *snode;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	fnode = head->next;
+	snode = (head->next)->next;
+
+	while (snode)
+	{
+		if (fnode == snode)
+		{
+			fnode = head;
+
+			while (fnode != snode)
+			{
+				fnode = fnode->next;
+				snode = snode->next;
+			}
+
+			return (fnode);
+		}
+
+		fnode = fnode->next;
+		nodes = (snode->next)->next;
+	}
+
+	return (NULL);
+}
